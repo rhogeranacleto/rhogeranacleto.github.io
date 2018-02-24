4d4f294127d152ca4745e6148efdd0ebe272ec0f
Rhoger Anacleto <rhogeranacleto@gmail.com>
2018-02-24 15:31:05 -0300

    Final adjustments on website

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,148 +1,29 @@
-var cards = [{
-        template: 'card-template-who-am-i',
-        title: 'Meu nome é Anacleto, Rhoger Anacleto',
-        body: '21 anos. Joinville, Brasil. Desenvolvedor.'
-    }, {
-        template: 'card-template-options-about-me',
-        title: 'Rhoger Anacleto, como é que eu posso explicar Rhoger Anacleto?',
-        opinions: [
-            'O Rhoger Anacleto é perfeito!',
-            'Ele tem duas bolsas de pele e um carro importado prateado',
-            'O cabelo dele está no seguro por US$10.000,00',
-            'Soube que ele faz comerciais de carro… no Japão',
-            'O filme que ele gosta mais é Marcação Cerrada',
-            'Ele até conheceu o John Stamos no avião… E ele disse que ele era lindo',
-            'Um dia, ele me socou no rosto… isso foi irado',
-        ]
-    }];
-var languages = [{
-        name: 'Javascript',
-        xp: 100,
-        width: 0,
-        class: ''
-    }, {
-        name: 'CSS',
-        xp: 100,
-        width: 0,
-        class: ''
-    }, {
-        name: 'HTML',
-        xp: 100,
-        width: 0,
-        class: ''
-    }, {
-        name: 'NodeJS',
-        xp: 96,
-        width: 0,
-        class: ''
-    }, {
-        name: 'Angular',
-        xp: 90,
-        width: 0,
-        class: ''
-    }, {
-        name: 'TypeScript',
-        xp: 87,
-        width: 0,
-        class: ''
-    }, {
-        name: 'C#',
-        xp: 70,
-        width: 0,
-        class: ''
-    }, {
-        name: 'EmberJS',
-        xp: 60,
-        width: 0,
-        class: ''
-    }, {
-        name: 'LESS',
-        xp: 70,
-        width: 0,
-        class: ''
-    }, {
-        name: 'GIT',
-        xp: 90,
-        width: 0,
-        class: ''
-    }, {
-        name: 'SQL',
-        xp: 95,
-        width: 0,
-        class: ''
-    }, {
-        name: 'C++',
-        xp: 10,
-        width: 0,
-        class: ''
-    }, {
-        name: 'PHP',
-        xp: 40,
-        width: 0,
-        class: ''
-    }, {
-        name: 'Java',
-        xp: 30,
-        width: 0,
-        class: ''
-    }, {
-        name: 'MongoDB',
-        xp: 85,
-        width: 0,
-        class: ''
-    }, {
-        name: 'GraphQL',
-        xp: 20,
-        width: 0,
-        class: ''
-    }, {
-        name: '.NET',
-        xp: 50,
-        width: 0,
-        class: ''
-    }, {
-        name: 'Selenium',
-        xp: 40,
-        width: 0,
-        class: ''
-    }, {
-        name: 'Photoshop',
-        xp: 60,
-        width: 0,
-        class: ''
-    }, {
-        name: 'shell',
-        xp: 20,
-        width: 0,
-        class: ''
-    }];
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var languages_1 = require("./languages");
 var chars = '1234567890!@#$%&*()qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
 $(function () {
-    var maxXP = languages.reduce(function (p, c) { return p.xp > c.xp ? p : c; }).xp;
-    var sumXP = languages.map(function (language) { return language.xp; }).reduce(function (sum, xp) { return sum + xp; });
+    var maxXP = languages_1.LANGUAGES.reduce(function (p, c) { return p.xp > c.xp ? p : c; }).xp;
+    var sumXP = languages_1.LANGUAGES.map(function (language) { return language.xp; }).reduce(function (sum, xp) { return sum + xp; });
     $('.sum-xp', $('#card-languade-xp')).text(sumXP + ' XP');
-    $.get('https://www.instagram.com/rhogeranacleto?__a=1').then(function (r) {
-        var media = r.user.media.nodes[0];
-        $('#card-instagram').find('img').attr('src', media.display_src).end().find('.caption>span').text(media.caption);
-        languages.sort(function (a, b) { return b.xp - a.xp; });
-        languages.forEach(function (language) {
-            language.width = 100 * (language.xp / maxXP);
-            language.class = "xp-bar-fill-" + parseInt(((language.width / 20) + 1).toString());
-            $('.body', $('#card-languade-xp')).append(Mustache.render($('#card-languade-xp-template').html(), language));
-        });
-        $('.card').each(function (i, card) {
-            var columnsContents = $('.column-content');
-            $(columnsContents.get().reduce(function (e, i) { return e.offsetHeight > i.offsetHeight ? i : e; }, columnsContents.get(0))).append(card);
-        });
-        var letterWidth = $('.bg', $('#content')).width();
-        var letterHeight = $('.bg', $('#content')).height();
-        var count = Math.floor($(document).width() / 8) * ((Math.floor($('#content').height() / 20)) + 1);
-        console.log(count, ((Math.floor($('#content').height() / 15)) + 1));
-        var string = '';
-        for (var i = 0; i < count; i++) {
-            string += chars.charAt(Math.floor(Math.random() * chars.length));
-            ;
-        }
-        $('.bg', $('#content')).html(string);
+    languages_1.LANGUAGES.sort(function (a, b) { return b.xp - a.xp; });
+    languages_1.LANGUAGES.forEach(function (language) {
+        language.width = 100 * (language.xp / maxXP);
+        language.class = "xp-bar-fill-" + parseInt(((language.width / 20) + 1).toString());
+        $('.body', $('#card-languade-xp')).append(Mustache.render($('#card-languade-xp-template').html(), language));
     });
+    $('.card').each(function (i, card) {
+        var columnsContents = $('.column-content');
+        $(columnsContents.get().reduce(function (e, i) { return e.offsetHeight > i.offsetHeight ? i : e; }, columnsContents.get(0))).append(card);
+    });
+    var letterWidth = $('.bg', $('#content')).width();
+    var letterHeight = $('.bg', $('#content')).height();
+    var count = Math.floor($(document).width() / 8) * ((Math.floor($('#content').height() / 20)) + 1);
+    var string = '';
+    for (var i = 0; i < count; i++) {
+        string += chars.charAt(Math.floor(Math.random() * chars.length));
+        ;
+    }
+    $('.bg', $('#content')).html(string);
 });
+console.log('What are search here? This code is public! Check out on https://github.com/rhogeranacleto/rhogeranacleto.github.io');
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,170 +1,52 @@
-const cards = [{
-	template: 'card-template-who-am-i',
-	title: 'Meu nome é Anacleto, Rhoger Anacleto',
-	body: '21 anos. Joinville, Brasil. Desenvolvedor.'
-}, {
-	template: 'card-template-options-about-me',
-	title: 'Rhoger Anacleto, como é que eu posso explicar Rhoger Anacleto?',
-	opinions: [
-		'O Rhoger Anacleto é perfeito!',
-		'Ele tem duas bolsas de pele e um carro importado prateado',
-		'O cabelo dele está no seguro por US$10.000,00',
-		'Soube que ele faz comerciais de carro… no Japão',
-		'O filme que ele gosta mais é Marcação Cerrada',
-		'Ele até conheceu o John Stamos no avião… E ele disse que ele era lindo',
-		'Um dia, ele me socou no rosto… isso foi irado',
-	]
-}];
-
-var languages = [{
-	name: 'Javascript',
-	xp: 100,
-	width: 0,
-	class: ''
-}, {
-	name: 'CSS',
-	xp: 100,
-	width: 0,
-	class: ''
-}, {
-	name: 'HTML',
-	xp: 100,
-	width: 0,
-	class: ''
-}, {
-	name: 'NodeJS',
-	xp: 96,
-	width: 0,
-	class: ''
-}, {
-	name: 'Angular',
-	xp: 90,
-	width: 0,
-	class: ''
-}, {
-	name: 'TypeScript',
-	xp: 87,
-	width: 0,
-	class: ''
-}, {
-	name: 'C#',
-	xp: 70,
-	width: 0,
-	class: ''
-}, {
-	name: 'EmberJS',
-	xp: 60,
-	width: 0,
-	class: ''
-}, {
-	name: 'LESS',
-	xp: 70,
-	width: 0,
-	class: ''
-}, {
-	name: 'GIT',
-	xp: 90,
-	width: 0,
-	class: ''
-}, {
-	name: 'SQL',
-	xp: 95,
-	width: 0,
-	class: ''
-}, {
-	name: 'C++',
-	xp: 10,
-	width: 0,
-	class: ''
-}, {
-	name: 'PHP',
-	xp: 40,
-	width: 0,
-	class: ''
-}, {
-	name: 'Java',
-	xp: 30,
-	width: 0,
-	class: ''
-}, {
-	name: 'MongoDB',
-	xp: 85,
-	width: 0,
-	class: ''
-}, {
-	name: 'GraphQL',
-	xp: 20,
-	width: 0,
-	class: ''
-}, {
-	name: '.NET',
-	xp: 50,
-	width: 0,
-	class: ''
-}, {
-	name: 'Selenium',
-	xp: 40,
-	width: 0,
-	class: ''
-}, {
-	name: 'Photoshop',
-	xp: 60,
-	width: 0,
-	class: ''
-}, {
-	name: 'shell',
-	xp: 20,
-	width: 0,
-	class: ''
-}];
+import { LANGUAGES } from "./languages";
 
 const chars = '1234567890!@#$%&*()qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
 
 $(function () {
 
-	var maxXP = languages.reduce((p, c) => p.xp > c.xp ? p : c).xp;
+	var maxXP = LANGUAGES.reduce((p, c) => p.xp > c.xp ? p : c).xp;
 
-	var sumXP = languages.map(language => language.xp).reduce((sum, xp) => sum + xp);
+	var sumXP = LANGUAGES.map(language => language.xp).reduce((sum, xp) => sum + xp);
 
 	$('.sum-xp', $('#card-languade-xp')).text(sumXP + ' XP');
 
-	$.get('https://www.instagram.com/rhogeranacleto?__a=1').then(r => {
+	// $.get('https://www.instagram.com/rhogeranacleto?__a=1').then(r => {
 
-		var media = r.user.media.nodes[0];
+	// var media = r.user.media.nodes[0];
 
-		$('#card-instagram').find('img').attr('src', media.display_src).end().find('.caption>span').text(media.caption);
+	// $('#card-instagram').find('img').attr('src', media.display_src).end().find('.caption>span').text(media.caption);
 
-		languages.sort((a, b) => b.xp - a.xp);
+	LANGUAGES.sort((a, b) => b.xp - a.xp);
 
-		languages.forEach(language => {
+	LANGUAGES.forEach(language => {
 
-			language.width = 100 * (language.xp / maxXP);
+		language.width = 100 * (language.xp / maxXP);
 
-			language.class = `xp-bar-fill-${parseInt(((language.width / 20) + 1).toString())}`;
+		language.class = `xp-bar-fill-${parseInt(((language.width / 20) + 1).toString())}`;
 
-			$('.body', $('#card-languade-xp')).append(Mustache.render($('#card-languade-xp-template').html(), language));
-		})
+		$('.body', $('#card-languade-xp')).append(Mustache.render($('#card-languade-xp-template').html(), language));
+	})
 
-		$('.card').each((i, card) => {
+	$('.card').each((i, card) => {
 
-			var columnsContents = $('.column-content');
-			$(columnsContents.get().reduce((e, i) => e.offsetHeight > i.offsetHeight ? i : e, columnsContents.get(0))).append(card);
-		});
-
-		var letterWidth = $('.bg', $('#content')).width();
-		var letterHeight = $('.bg', $('#content')).height();
-
-		const count = Math.floor($(document).width() / 8) * ((Math.floor($('#content').height() / 20)) + 1);
-
-		console.log(count, ((Math.floor($('#content').height() / 15)) + 1));
-
-		var string = '';
-
-		for (let i = 0; i < count; i++) {
-
-			string += chars.charAt(Math.floor(Math.random() * chars.length));;
-		}
-
-		$('.bg', $('#content')).html(string);
+		var columnsContents = $('.column-content');
+		$(columnsContents.get().reduce((e, i) => e.offsetHeight > i.offsetHeight ? i : e, columnsContents.get(0))).append(card);
 	});
+
+	var letterWidth = $('.bg', $('#content')).width();
+	var letterHeight = $('.bg', $('#content')).height();
+
+	const count = Math.floor($(document).width() / 8) * ((Math.floor($('#content').height() / 20)) + 1);
+
+	var string = '';
+
+	for (let i = 0; i < count; i++) {
+
+		string += chars.charAt(Math.floor(Math.random() * chars.length));;
+	}
+
+	$('.bg', $('#content')).html(string);
+	// });
 });
+
+console.log('What are search here? This code is public! Check out on https://github.com/rhogeranacleto/rhogeranacleto.github.io');
